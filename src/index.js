18a8ac70c1838270bdfa0263653bcaff6edccd54
poonsht <poon_arsene_lupin@hotmail.com>
2020-04-12 12:01:13 +0700

    [-] fix : move helmet to app.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,4 @@
 import "./style";
-import { h } from "preact";
 import App from "./components/app";
 
-const HOChelmet = () => {
-  return (
-    <>
-      <link
-        href="https://fonts.googleapis.com/css?family=Kanit&display=swap"
-        rel="stylesheet"
-      />
-      <App />
-    </>
-  );
-};
-
-export default HOChelmet;
+export default App;
