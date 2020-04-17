1b6676c05cef6da60638f639c6e5f946f0cfe1bc
poonsht <poon_arsene_lupin@hotmail.com>
2020-04-17 15:16:06 +0700

    [+] add : apis for connect backend, example for using

--- src/components/common/ReviewCard.js
+++ src/components/common/ReviewCard.js
@@ -1,175 +1,194 @@
 import styled from "styled-components";
-import { Clap, Boo } from '../../assets/icons/Icons'
-import { useState } from 'preact/hooks';
-import { Popover } from '@material-ui/core';
+import { Clap, Boo } from "../../assets/icons/Icons";
+import { useState } from "preact/hooks";
+import { Popover } from "@material-ui/core";
 
 const Container = styled.div`
-    border: 0.2rem solid #E0E0E0;
-    border-radius: 1rem;
-    margin: 2rem 0;
-    padding: 1.2rem 1.6rem;
+  border: 0.2rem solid #e0e0e0;
+  border-radius: 1rem;
+  margin: 2rem 0;
+  padding: 1.2rem 1.6rem;
 `;
 
 const Content = styled.div`
-    font-size: 1.7rem;
-    color: #4F4F4F;
+  font-size: 1.7rem;
+  color: #4f4f4f;
 `;
 
 const DetailsContainer = styled.div`
-    font-size: 1.6rem;
-    display: flex;
-    color: #828282;
-    margin-top: 0.8rem;
-    justify-content: space-between;
-`
+  font-size: 1.6rem;
+  display: flex;
+  color: #828282;
+  margin-top: 0.8rem;
+  justify-content: space-between;
+`;
 
 const DetailRight = styled.div`
-    display: flex;
-    width: 14rem;
-    justify-content: space-between;
-`
+  display: flex;
+  width: 14rem;
+  justify-content: space-between;
+`;
 
 const Button = styled.div`
-    display: flex;
-    width: ${props => props.type === "report" ? '6rem' : 'auto'};
-    text-align: right;
-    align-items: center;
-    justify-content: ${props => props.type === "report" ? 'flex-end' : 'space-between'};
-    user-select: none;
-    cursor: pointer;
+  display: flex;
+  width: ${(props) => (props.type === "report" ? "6rem" : "auto")};
+  text-align: right;
+  align-items: center;
+  justify-content: ${(props) =>
+    props.type === "report" ? "flex-end" : "space-between"};
+  user-select: none;
+  cursor: pointer;
 
-    &:hover {
-        color: #9AC1EE;
-        svg {
-            #clap {
-                fill: #9AC1EE;
-            }
-            #boo {
-                fill: #EEA99A;
-            }
-        }
+  &:hover {
+    color: #9ac1ee;
+    svg {
+      #clap {
+        fill: #9ac1ee;
+      }
+      #boo {
+        fill: #eea99a;
+      }
     }
+  }
 
-    &:active {
-        svg {
-            #clap {
-                fill: #2F80ED;
-            }
-            #boo {
-                fill: #EB5757;
-            }
-        }
+  &:active {
+    svg {
+      #clap {
+        fill: #2f80ed;
+      }
+      #boo {
+        fill: #eb5757;
+      }
     }
-`
+  }
+`;
 
 const ButtonLeft = styled.div`
-    display: flex;
-    width: 14rem;
-    justify-content: space-between;
-    align-items: center;
-    color: #BDBDBD;
+  display: flex;
+  width: 14rem;
+  justify-content: space-between;
+  align-items: center;
+  color: #bdbdbd;
 
-    span {
-        user-select: none;
-        width: 2rem;
-    }
-`
+  span {
+    user-select: none;
+    width: 2rem;
+  }
+`;
 
 const ModalBackdrop = styled.div`
-    position: fixed;
-    height: 100%;
-    width: 100%;
-    top: 0;
-    left: 0;
-    background-color: hsla(10, 10%, 10%, 50%);
-    display: ${props => props.show === true? 'block' : 'none'};
-    z-index: 1;
-    cursor: pointer;
-`
+  position: fixed;
+  height: 100%;
+  width: 100%;
+  top: 0;
+  left: 0;
+  background-color: hsla(10, 10%, 10%, 50%);
+  display: ${(props) => (props.show === true ? "block" : "none")};
+  z-index: 1;
+  cursor: pointer;
+`;
 
 const Modal = styled.div`
-    border-radius: 10px;
-    background-color: white;
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    padding: 2.8rem 1.2rem;
-    font-weight: 500;
-    font-size: 2rem;
-    line-height: 3.4rem;
-    text-align: center;
-    display: ${props => props.show === true? 'block' : 'none'};
-    z-index: 1;
-    max-width: 42rem;
-    width: 84%;
-`
+  border-radius: 10px;
+  background-color: white;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  padding: 2.8rem 1.2rem;
+  font-weight: 500;
+  font-size: 2rem;
+  line-height: 3.4rem;
+  text-align: center;
+  display: ${(props) => (props.show === true ? "block" : "none")};
+  z-index: 1;
+  max-width: 42rem;
+  width: 84%;
+`;
 
 const ModalActions = styled.div`
-    align-self: center;
-    padding: 1.4rem 0 0; 
-    display: flex;
-    flex-direction: row;
-    flex-flow: wrap;
-    justify-content: center;
-`
+  align-self: center;
+  padding: 1.4rem 0 0;
+  display: flex;
+  flex-direction: row;
+  flex-flow: wrap;
+  justify-content: center;
+`;
 
-const ConfirmButton = styled.div `
-    background-color: #EB5757;
-    align-self: center;
-    color: #fff;
-    padding: 0.4rem 2.4rem;
-    border-radius: 0.6rem;
-    font-size: 2rem;
-    font-weight: 500;
-    cursor: pointer;
-    width: 12.2rem;
-    margin: 1rem 1rem;
+const ConfirmButton = styled.div`
+  background-color: #eb5757;
+  align-self: center;
+  color: #fff;
+  padding: 0.4rem 2.4rem;
+  border-radius: 0.6rem;
+  font-size: 2rem;
+  font-weight: 500;
+  cursor: pointer;
+  width: 12.2rem;
+  margin: 1rem 1rem;
 `;
 
 const CancelButton = styled(ConfirmButton)`
-    padding: 0.4rem 1.2rem;
-    font-size: 2rem;
-    text-align: center;
-    background-color: #BDBDBD;
-`
+  padding: 0.4rem 1.2rem;
+  font-size: 2rem;
+  text-align: center;
+  background-color: #bdbdbd;
+`;
 
-const ReviewCard = () => {
-    const [actions, setActions] = useState({
-        clap: 10,
-        boo: 2,
-    })
-    const [showDialog, setDialog] = useState(false);
+const ReviewCard = (props) => {
+  const { reviewId, text, clap, boo, grade, author, createdAt } = props;
+  const [actions, setActions] = useState({
+    clap: clap,
+    boo: boo,
+  });
+  const [showDialog, setDialog] = useState(false);
 
-    return (
-        <Container>
-            <Content> การบ้านน้อยมากกกบ 8 ชม. ได้ A เย้ </Content>
-            <DetailsContainer>
-                โดย ASMB
-                <DetailRight>
-                    <span>เกรด A</span>
-                    <span>11 เม.ย. 19</span>
-                </DetailRight>
-            </DetailsContainer>
-            <DetailsContainer>
-                <ButtonLeft>
-                    <Button onClick={() => setActions({...actions, clap: actions.clap+1})}><Clap /></Button>
-                    <span>{actions.clap}</span>
-                    <Button onClick={() => setActions({...actions, boo: actions.boo+1})}><Boo /></Button>
-                    <span>{actions.boo}</span>
-                </ButtonLeft>
-                <Button type="report" onClick={() => setDialog(true)}>Report</Button>
-            </DetailsContainer>
-            <ModalBackdrop show={showDialog} onClick={() => setDialog(false)} />
-            <Modal show={showDialog}>
-                แจ้งลบรีวิวหรือไม่ ?
-                <ModalActions>
-                <CancelButton onClick={() => setDialog(false)}>ยกเลิก</CancelButton>
-                <ConfirmButton>แจ้งลบ</ConfirmButton>
-                </ModalActions>
-            </Modal>
-        </Container>
-    );
+  const pareDate = (dateUTC) => {
+    // REQUIR implement parser for better ux (convert utc timezone)
+    // 2020-04-15T01:25:52.150+00:00 => 15 เม.ษ 2020
+    let date = dateUTC;
+    return date;
+  };
+
+  return (
+    <Container>
+      <Content> {text}</Content>
+      <DetailsContainer>
+        โดย {author}
+        <DetailRight>
+          <span>เกรด {grade}</span>
+          <span>{pareDate(createdAt)}</span>
+        </DetailRight>
+      </DetailsContainer>
+      <DetailsContainer>
+        <ButtonLeft>
+          <Button
+            onClick={() => setActions({ ...actions, clap: actions.clap + 1 })}
+          >
+            <Clap />
+          </Button>
+          <span>{actions.clap}</span>
+          <Button
+            onClick={() => setActions({ ...actions, boo: actions.boo + 1 })}
+          >
+            <Boo />
+          </Button>
+          <span>{actions.boo}</span>
+        </ButtonLeft>
+        <Button type="report" onClick={() => setDialog(true)}>
+          Report
+        </Button>
+      </DetailsContainer>
+      <ModalBackdrop show={showDialog} onClick={() => setDialog(false)} />
+      <Modal show={showDialog}>
+        แจ้งลบรีวิวหรือไม่ ?
+        <ModalActions>
+          <CancelButton onClick={() => setDialog(false)}>ยกเลิก</CancelButton>
+          <ConfirmButton>แจ้งลบ</ConfirmButton>
+        </ModalActions>
+      </Modal>
+    </Container>
+  );
 };
 
 export default ReviewCard;
--- src/components/utillity/apis.js
+++ src/components/utillity/apis.js
@@ -1,22 +1,83 @@
 import axios from "axios";
 
 const URL_API = "https://kuclap-api.herokuapp.com";
+const api = axios.create({
+  baseURL: URL_API,
+});
 
-export const apis = {
+const apis = {
+  // GET tail reviews when user load page.
   getLastReviews: async (offset, next) => {
     try {
-      const res = await axios.get(`${URL_API}/reviews/last?offset=${offset}`);
+      const res = await api.get(`/reviews/last?offset=${offset}`);
       next(res);
     } catch (err) {
       console.log(err);
     }
   },
+  // GET data classes when componentwillmount (on hook).
   getAllClasses: async (next) => {
     try {
-      const res = axios.get(`${URL_API}/classes`);
+      const res = await api.get(`/classes`);
       next(res);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  // GET stats when class selected.
+  getClassDetailByClassId: async (classid, next) => {
+    try {
+      const res = await api.get(`/classes/${classid}`);
+      next(res);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  // POST & PUT create review then update stats class
+  createReview: async (payloadReview, payloadVote) => {
+    try {
+      const resR = await api.post(`/review`, payloadReview);
+      const resV = await api.put(`/class/${payloadReview}/stats`, payloadVote);
+      console.log(resR, resV);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  // GET reviews by classid when class selected
+  getReviewsByClassId: async (classid, next) => {
+    try {
+      const res = await api.get(`/reviews/${classid}`);
+      next(res);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  // PUT report review by reviewid
+  putReportReviewByReviewId: async (reviewid) => {
+    try {
+      const res = await api.put(`/review/report/${reviewid}`);
+      console.log(res);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  // PUT clap review by reviewid
+  putClapReviewByReviewId: async (reviewid, clap) => {
+    try {
+      const res = await api.put(`/review/clap/${reviewid}/${clap}`);
+      console.log(res);
+    } catch (err) {
+      console.log(err);
+    }
+  },
+  // PUT boo review by reviewid
+  putBooReviewByReviewId: async (reviewid, boo) => {
+    try {
+      const res = await api.put(`/review/boo/${reviewid}/${boo}`);
+      console.log(res);
     } catch (err) {
       console.log(err);
     }
   },
 };
+export default apis;
