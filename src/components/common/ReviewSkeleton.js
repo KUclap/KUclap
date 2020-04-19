292c99c15ad1dd0bb41c5737aedd7294ce273cc5
ASMB <asmb.dwr@gmail.com>
2020-04-19 23:22:49 +0700

    feat(review-card): edit skeleton

--- src/components/common/ReviewSkeleton.js
+++ src/components/common/ReviewSkeleton.js
@@ -12,61 +12,73 @@ const ReviewSkeleton = () => (
   <Container>
     <svg
       role="img"
-      width="55%"
-      height="131"
+      width="95%"
+      height="176"
       aria-labelledby="loading-aria"
-      viewBox="0 0 318 131"
+      viewBox="0 0 318 176"
       preserveAspectRatio="none"
     >
-      <title id="loading-aria">Loading...</title>
-      <rect
-        x="0"
-        y="0"
-        width="100%"
-        height="100%"
-        clip-path="url(#clip-path)"
-        style='fill: url("#fill");'
-      ></rect>
-      <defs>
-        <clipPath id="clip-path">
-          <rect x="0" y="0" rx="7" ry="7" width="318" height="15" />
-          <rect x="0" y="21" rx="7" ry="7" width="171" height="15" />
-          <rect x="0" y="42" rx="7" ry="7" width="211" height="15" />
-          <rect x="0" y="83" rx="7" ry="7" width="75" height="11" />
-          <rect x="0" y="101" rx="7" ry="7" width="109" height="11" />
-          <rect x="1" y="119" rx="7" ry="7" width="60" height="11" />
-        </clipPath>
-        <linearGradient id="fill">
-          <stop offset="0.599964" stop-color="#f3f3f3" stop-opacity="1">
-            <animate
-              attributeName="offset"
-              values="-2; -2; 1"
-              keyTimes="0; 0.25; 1"
-              dur="2s"
-              repeatCount="indefinite"
-            ></animate>
-          </stop>
-          <stop offset="1.59996" stop-color="#ecebeb" stop-opacity="1">
-            <animate
-              attributeName="offset"
-              values="-1; -1; 2"
-              keyTimes="0; 0.25; 1"
-              dur="2s"
-              repeatCount="indefinite"
-            ></animate>
-          </stop>
-          <stop offset="2.59996" stop-color="#f3f3f3" stop-opacity="1">
-            <animate
-              attributeName="offset"
-              values="0; 0; 3"
-              keyTimes="0; 0.25; 1"
-              dur="2s"
-              repeatCount="indefinite"
-            ></animate>
-          </stop>
-        </linearGradient>
-      </defs>
-    </svg>
+   <title id="loading-aria">Loading...</title>
+    <rect
+      x="0"
+      y="0"
+      width="100%"
+      height="100%"
+      clip-path="url(#clip-path)"
+      style='fill: url("#fill");'
+    ></rect>
+    <defs>
+      <clipPath id="clip-path">
+          <rect x="8" y="156" rx="3" ry="3" width="68" height="12" /> 
+          <rect x="8" y="132" rx="3" ry="3" width="88" height="12" /> 
+          <rect x="8" y="106" rx="3" ry="3" width="52" height="12" /> 
+          <rect x="8" y="40" rx="3" ry="3" width="90%" height="12" /> 
+          <rect x="8" y="68" rx="3" ry="3" width="40%" height="12" /> 
+          <rect x="8" y="12" rx="3" ry="3" width="90%" height="12" /> 
+      </clipPath>
+      <linearGradient id="fill">
+        <stop
+          offset="0.599964"
+          stop-color="#f3f3f3"
+          stop-opacity="1"
+        >
+          <animate
+            attributeName="offset"
+            values="-2; -2; 1"
+            keyTimes="0; 0.25; 1"
+            dur="2s"
+            repeatCount="indefinite"
+          ></animate>
+        </stop>
+        <stop
+          offset="1.59996"
+          stop-color="#ecebeb"
+          stop-opacity="1"
+        >
+          <animate
+            attributeName="offset"
+            values="-1; -1; 2"
+            keyTimes="0; 0.25; 1"
+            dur="2s"
+            repeatCount="indefinite"
+          ></animate>
+        </stop>
+        <stop
+          offset="2.59996"
+          stop-color="#f3f3f3"
+          stop-opacity="1"
+        >
+          <animate
+            attributeName="offset"
+            values="0; 0; 3"
+            keyTimes="0; 0.25; 1"
+            dur="2s"
+            repeatCount="indefinite"
+          ></animate>
+        </stop>
+      </linearGradient>
+    </defs>
+  </svg>
   </Container>
 );
 
