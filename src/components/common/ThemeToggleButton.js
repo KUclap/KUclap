b8351c022f02be796fbb554c066a068806330357
Anawin W <imonmac@hotmail.com>
2020-05-06 02:25:00 +0700

    [+] change : ThemeToggle border on breakpoints

--- src/components/common/ThemeToggleButton.js
+++ src/components/common/ThemeToggleButton.js
@@ -4,15 +4,16 @@ import { Dark, Light } from "../utillity/Icons";
 
 const Button = styled.div`
   background: white;
-  border: 2px solid ${(props) => props.theme.bodyText};
 
   width: 72px;
   height: 36px;
   border-radius: 18px;
+  border: 2px solid ${(props) => props.theme.bodyText};
   
   ${media.lessThan("medium")`
     width: 56px;
     height: 28px;
+    border-width: 1.5px;
   `}
 
   ${media.lessThan("small")`
@@ -28,21 +29,23 @@ const Button = styled.div`
   ` : ''}
 `;
 const Circle = styled.div`
-  border: 2px solid black;
-  margin: 2px;
   background ${(props) => props.theme.body};
   float: ${(props) => props.theme.name === 'dark' ? 'right' : 'left'};
 
+  margin: 2px;
   padding: 4px;
   width: 28px;
   height: 28px;
   border-radius: 14px;
+  border: 2px solid black;
 
   ${media.lessThan("medium")`
+    margin: 2.5px;
     padding: 2px;
     width: 20px;
     height: 20px;
     border-radius: 10px;
+    border-width: 1.5px;
   `}
 
   ${media.lessThan("small")`
