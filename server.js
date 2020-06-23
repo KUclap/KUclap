1249ad2d265ed5aec54f60332ac00d9bc5b15c25
poonsht <poon_arsene_lupin@hotmail.com>
2020-06-23 15:22:33 +0700

    [+] remove : unuse on server

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-// const { ServerStyleSheet, StyleSheetManager } = require("styled-components");
 const sirv = require("sirv");
 const polka = require("polka");
 const { h } = require("preact");
@@ -28,10 +27,6 @@ polka()
   .use(sirv("build", { setHeaders }))
   .get("*", (req, res) => {
     let body = render(h(App, { url: req.url }));
-    // const sheet = new ServerStyleSheet();
-    // let body = render(sheet.collectStyles(h(App, { url: req.url })));
-    // const styleTags = sheet.getStyleTags(); // sheet.getStyleElement();
-    // sheet.seal();
     res.setHeader("Content-Type", "text/html");
     console.log(template.replace(RGX, body));
     res.end(template.replace(RGX, body));
@@ -40,31 +35,3 @@ polka()
     if (err) throw err;
     console.log(`> Running on localhost:${PORT}`);
   });
-
-//"use strict";
-
-// const express = require("express");
-// const { h } = require("preact");
-// const renderToString = require("preact-render-to-string");
-// const { App } = require("./src/components/app");
-// const path = require("path");
-
-// const app = express();
-
-// app.use(express.static(path.join(__dirname, "dist")));
-
-// app.listen(8080);
-
-// app.get("*", (req, res) => {
-//   const html = renderToString(h(App, { url: req.url }));
-
-//   res.send(`
-// <!DOCTYPE html>
-// <html>
-//     <body>
-//         <div id="app">${html}</div>
-//         <script src="./app.js"></script>
-//     </body>
-// </html>
-//     `);
-// });
