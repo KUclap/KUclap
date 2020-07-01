b513f68ef90024abea68bfa196b166c84d6959c6
poonsht <poon_arsene_lupin@hotmail.com>
2020-07-01 16:03:20 +0700

    [+] add : caller Helmet component and remove class fetcher

--- src/components/common/PageTemplate.js
+++ src/components/common/PageTemplate.js
@@ -1,7 +1,6 @@
 import { h } from "preact";
 import { route } from "preact-router";
 import { useState, useEffect, useRef, useContext } from "preact/hooks";
-import Helmet from "preact-helmet";
 import Select from "react-virtualized-select";
 import styled, { css, withTheme } from "styled-components";
 
@@ -13,10 +12,10 @@ import { GoToTop } from "../utility/Icons";
 import { ModalContext } from "../../context/ModalContext";
 import { ReviewFetcherContext } from "../../context/ReviewFetcherContext";
 import { SelectContext } from "../../context/SelectContext";
-import APIs from "../utility/apis";
 import baseroute from "../utility/baseroute";
 import GlobalComponent from "../utility/GlobalComponent";
 import Header from "./Header";
+import Helmet from "./Helmet";
 
 const Container = styled.div`
   display: flex;
@@ -73,6 +72,7 @@ const PageTemplate = ({
   toggleTheme,
   children,
   isFormPage,
+  classes,
   content,
 }) => {
   const { state: selected, dispatch: dispatchSelected } = useContext(
@@ -81,40 +81,8 @@ const PageTemplate = ({
   const { state: modal } = useContext(ModalContext);
 
   const newEle = useRef(null);
-  const [classes, setClasses] = useState([]);
   const [isBottomViewport, setIsBottomViewport] = useState(false);
-  const {
-    setReviews,
-    setLoading,
-    paging,
-    setScore,
-    setUnderFlow,
-    setPaging,
-  } = useContext(ReviewFetcherContext);
-
-  const handleFetchingReviewsAndClass = (classId) => {
-    setPaging({ ...paging, page: 1 });
-    setReviews([]);
-    setLoading(true);
-    setUnderFlow(false);
-    APIs.getReviewsByClassId(classId, 0, paging.offset, (res) => {
-      if (res.data === null) {
-        setUnderFlow(true);
-      } else {
-        setReviews(res.data);
-        setUnderFlow(false);
-      }
-      setLoading(false);
-    });
-
-    APIs.getClassDetailByClassId(classId, (res) => {
-      setScore({
-        homework: res.data.stats.homework,
-        interest: res.data.stats.interest,
-        how: res.data.stats.how,
-      });
-    });
-  };
+  const { handleFetchingReviewsAndClass } = useContext(ReviewFetcherContext);
 
   const handleSelected = (e) => {
     if (!isFormPage) {
@@ -129,16 +97,13 @@ const PageTemplate = ({
   };
 
   useEffect(() => {
-    APIs.getAllClasses((res) => {
-      setClasses(res.data);
-      if (!classID)
-        dispatchSelected({
-          type: "change_label",
-          value: {
-            label: "ค้นหาวิชาด้วยรหัสวิชา ชื่อวิชาภาษาไทย / ภาษาอังกฤษ",
-          },
-        });
-    });
+    if (!classID)
+      dispatchSelected({
+        type: "change_label",
+        value: {
+          label: "ค้นหาวิชาด้วยรหัสวิชา ชื่อวิชาภาษาไทย / ภาษาอังกฤษ",
+        },
+      });
   }, []);
 
   useEffect(() => {
@@ -159,39 +124,7 @@ const PageTemplate = ({
 
   return (
     <Container name="top">
-      <Helmet
-        htmlAttributes={{ lang: "th", amp: undefined }} // amp takes no value
-        title="KUclap"
-        titleTemplate={`KUclap: ${content.title} - KUclap`}
-        defaultTitle="KUclap : เว็บไซต์ค้นหาและแบ่งปันรีวิววิชาบูรณาการ มก."
-        titleAttributes={{ itemprop: "name", lang: "th" }}
-        //base={{ target: "_blank", href: "https://kuclap.com/" }}
-        meta={[
-          // Robot
-          // { name: "robots", content: "noindex" },
-          // Primary Meta Tags
-          { name: "title", content: `KUclap : ${content.title}` },
-          { name: "description", content: `${content.description} - KUclap` },
-          // Open Graph / Facebook
-          { property: "og:type", content: "website" },
-          { property: "og:url", content: "https://kuclap.com/" },
-          { property: "og:title", content: `KUclap ${content.title}` },
-          {
-            property: "og:description",
-            content: `${content.description} - KUclap`,
-          },
-          { property: "og:image", content: content.image },
-          // Twitter
-          { property: "twitter:card", content: "summary_large_image" },
-          { property: "twitter:url", content: "https://kuclap.com/" },
-          { property: "twitter:title", content: `KUclap ${content.title}` },
-          {
-            property: "twitter:description",
-            content: `${content.description} - KUclap`,
-          },
-          { property: "twitter:image", content: content.image },
-        ]}
-      />
+      <Helmet content={content} />
       <GlobalComponent isOverflow={modal.showModal} />
       <GoTopCustomStyle isBottomViewport={isBottomViewport} href="#top">
         <GoToTop />
