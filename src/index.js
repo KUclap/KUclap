1a92e93c1be28504bfefbb33490b89947f493f55
poonsht <poon_arsene_lupin@hotmail.com>
2020-07-02 11:11:56 +0700

    [+] comment : unuse import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import { h, render } from "preact";
 import App from "./app";
 import withStyledComponentSSR from "./HOC/withStyledComponentSSR";
-import withClasses from "./HOC/withClasses";
+// import withClasses from "./HOC/withClasses";
 
 // export default withStyledComponentSSR(App);
 
