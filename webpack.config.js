1345a661af21b18c348859b64ab2d21efe9a153e
poonsht <poon_arsene_lupin@hotmail.com>
2020-09-28 23:56:42 +0700

    [+] update : preact version (10.4.8wq)

--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,6 @@ module.exports = {
   resolve: {
     alias: {
       react: "preact/compat",
-      react: "preact/compat",
       "react-dom/test-utils": "preact/test-utils",
       "react-dom": "preact/compat",
       React: "preact/compat",
