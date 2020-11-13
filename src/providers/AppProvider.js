d29067370013213a070ec3921786c403457f5bf3
poonsht <poon_arsene_lupin@hotmail.com>
2020-11-14 01:15:37 +0700

    [+] add : mounted for handle dark-mode: this hack make load page like csr

--- src/providers/AppProvider.js
+++ src/providers/AppProvider.js
@@ -1,18 +1,29 @@
 import { h } from "preact";
-
+import { useState, useEffect} from 'preact/hooks'
 import { ThemeProvider } from "styled-components";
 
 import { ModalProvider } from "../context/ModalContext";
 import { SelectProvider } from "../context/SelectContext";
 
 const AppProvider = ({ children, theme }) => {
-  return (
+  const [mounted, setMounted] = useState(false)
+  
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  const app = 
     <ModalProvider>
       <SelectProvider>
-        <ThemeProvider theme={theme}>{children}</ThemeProvider>
+          <ThemeProvider theme={theme}>{children}</ThemeProvider>
       </SelectProvider>
     </ModalProvider>
-  );
+  
+  if(!mounted){
+    return <div style={{visibility: 'hidden'}}>{app}</div>
+  }
+
+  return app
 };
 
 export default AppProvider;
