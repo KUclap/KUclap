afdc7275bca2301699780027ae297faa1142eccb
poonsht <poon_arsene_lupin@hotmail.com>
2020-11-28 01:20:57 +0700

    [-] del : unuse code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
+import './assets/css/style.css'
 import App from "./app";
 import withStyledComponentSSR from "./HOC/withStyledComponentSSR";
 
-// export default function (initData){
-    // return withStyledComponentSSR(<App initData={initData} />);
-// }
-
 export default withStyledComponentSSR(App);
-
-// if (typeof window !== "undefined")
-//   render(
-//     withStyledComponentSSR(<App />),
-//     document.body,
-//     document.getElementById("app")
-//   );
-
-// export default App;
