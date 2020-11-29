78775e7839ad4e66d1e754baf114c2edd707182c
poonsht <poon_arsene_lupin@hotmail.com>
2020-11-29 17:11:17 +0700

    [+] refactor : use factory for create helmet data on prerendering and client rendering

--- src/prerender-urls.js
+++ src/prerender-urls.js
@@ -1,79 +1,32 @@
+const { 
+  defaultTitle,
+  defaultMeta,
+  defaultPreRenderingData
+  } = require('./components/utility/helmet')
+
 module.exports = async function () {
-  // console.log(pages);
   const index = [
     {
       url: "/",
-      title: ``,
-      meta: {
-        title: `KUclap - เว็บไซต์ค้นหาและแบ่งปันรีวิววิชาบูรณาการ มก.`,
-        description: `kuclap.com แหล่งรวม ค้นหารีวิว เขียนรีวิว คำแนะนำ วิชาบูรณาการ วิชาบูร วิชาบูรฯ วิชาเสรี วิชาเลือก วิชาศึกษาทั่วไป รีวิว หน่วยกิต ชั่วโมงเรียน อาจารย์ การบ้าน ม.เกษตร มหาวิทยาลัยเกษตรศาสตร์ มก. KU - KUclap`,
-      },
-      data: {
-        type: `website`,
-        card: `summary_large_image`,
-        url: `https://www.kuclap.com/`,
-        title: `KUclap - เว็บไซต์ค้นหาและแบ่งปันรีวิววิชาบูรณาการ มก.`,
-        description: `kuclap.com แหล่งรวม ค้นหารีวิว เขียนรีวิว คำแนะนำ วิชาบูรณาการ วิชาบูร วิชาบูรฯ วิชาเสรี วิชาเลือก วิชาศึกษาทั่วไป รีวิว หน่วยกิต ชั่วโมงเรียน อาจารย์ การบ้าน ม.เกษตร มหาวิทยาลัยเกษตรศาสตร์ มก. KU - KUclap`,
-        image: "https://www.kuclap.com/assets/img/meta-og-image.png",
-      },
+      title: defaultTitle,
+      meta: defaultMeta,
+      data: defaultPreRenderingData,
     },
     {
       url: "/class",
-      title: ``,
       isClassPage: true,
-      meta: {
-        title: `KUclap - เว็บไซต์ค้นหาและแบ่งปันรีวิววิชาบูรณาการ มก.`,
-        description: `kuclap.com แหล่งรวม ค้นหารีวิว เขียนรีวิว คำแนะนำ วิชาบูรณาการ วิชาบูร วิชาบูรฯ วิชาเสรี วิชาเลือก วิชาศึกษาทั่วไป รีวิว หน่วยกิต ชั่วโมงเรียน อาจารย์ การบ้าน ม.เกษตร มหาวิทยาลัยเกษตรศาสตร์ มก. KU - KUclap`,
-      },
-      data: {
-        type: `website`,
-        card: `summary_large_image`,
-        url: `https://www.kuclap.com/`,
-        title: `KUclap - เว็บไซต์ค้นหาและแบ่งปันรีวิววิชาบูรณาการ มก.`,
-        description: `kuclap.com แหล่งรวม ค้นหารีวิว เขียนรีวิว คำแนะนำ วิชาบูรณาการ วิชาบูร วิชาบูรฯ วิชาเสรี วิชาเลือก วิชาศึกษาทั่วไป รีวิว หน่วยกิต ชั่วโมงเรียน อาจารย์ การบ้าน ม.เกษตร มหาวิทยาลัยเกษตรศาสตร์ มก. KU - KUclap`,
-        image: "https://www.kuclap.com/assets/img/meta-og-image.png",
-      },
+      title: defaultTitle,
+      meta: defaultMeta,
+      data: defaultPreRenderingData,
     },
     {
       url: "/review",
-      title: ``,
       isReviewPage: true,
-      meta: {
-        title: `KUclap - เว็บไซต์ค้นหาและแบ่งปันรีวิววิชาบูรณาการ มก.`,
-        description: `kuclap.com แหล่งรวม ค้นหารีวิว เขียนรีวิว คำแนะนำ วิชาบูรณาการ วิชาบูร วิชาบูรฯ วิชาเสรี วิชาเลือก วิชาศึกษาทั่วไป รีวิว หน่วยกิต ชั่วโมงเรียน อาจารย์ การบ้าน ม.เกษตร มหาวิทยาลัยเกษตรศาสตร์ มก. KU - KUclap`,
-      },
-      data: {
-        type: `website`,
-        card: `summary_large_image`,
-        url: `https://www.kuclap.com/`,
-        title: `KUclap - เว็บไซต์ค้นหาและแบ่งปันรีวิววิชาบูรณาการ มก.`,
-        description: `kuclap.com แหล่งรวม ค้นหารีวิว เขียนรีวิว คำแนะนำ วิชาบูรณาการ วิชาบูร วิชาบูรฯ วิชาเสรี วิชาเลือก วิชาศึกษาทั่วไป รีวิว หน่วยกิต ชั่วโมงเรียน อาจารย์ การบ้าน ม.เกษตร มหาวิทยาลัยเกษตรศาสตร์ มก. KU - KUclap`,
-        image: "https://www.kuclap.com/assets/img/meta-og-image.png",
-      },
+      title: defaultTitle,
+      meta: defaultMeta,
+      data: defaultPreRenderingData,
     },
   ];
 
   return index;
-
-  // Data for prerender /:classId route
-
-  // const response = await fetch(
-  //   `https://kuclap-api-staging.herokuapp.com/classes`
-  // );
-  // const pages = await response.json();
-  // return pages
-  //   .map((page) => ({
-  //     url: `/class/${page.classId}`,
-  //     title: `รีวิววิชา ${page.nameTh} ${page.nameEn} (${page.classId}) มก. - KUclap`,
-  //     meta: {
-  //       title: `รีวิววิชา ${page.nameTh} ${page.nameEn} (${page.classId}) มก. - KUclap`,
-  //       description: `รีวิววิชา ${page.label} - แหล่งรวม ค้นหารีวิว เขียนรีวิว คำแนะนำ วิชาบูรณาการ วิชาบูร วิชาบูรฯ วิชาเสรี วิชาเลือก วิชาศึกษาทั่วไป รีวิว หน่วยกิต ชั่วโมงเรียน อาจารย์ การบ้าน ม.เกษตร มหาวิทยาลัยเกษตรศาสตร์ มก. KU - KUclap`,
-  //     },
-  //     data: {
-  //       title: `รีวิววิชา ${page.nameTh} ${page.nameEn} (${page.classId}) มก. - KUclap`,
-  //       description: `รีวิววิชา ${page.label} - แหล่งรวม ค้นหารีวิว เขียนรีวิว คำแนะนำ วิชาบูรณาการ วิชาบูร วิชาบูรฯ วิชาเสรี วิชาเลือก วิชาศึกษาทั่วไป รีวิว หน่วยกิต ชั่วโมงเรียน อาจารย์ การบ้าน ม.เกษตร มหาวิทยาลัยเกษตรศาสตร์ มก. KU - KUclap`,
-  //       image: "https://www.kuclap.com/assets/img/meta-og-image.png",
-  //     },
-  //   }))
-  //   .concat(index);
 };
