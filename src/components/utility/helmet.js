2b212b6b0b384540391e511c3ad1003762256e26
aslupin <poon_arsene_lupin@hotmail.com>
2021-04-17 17:33:29 +0700

    [+] fix : change title sign when laoding state

--- src/components/utility/helmet.js
+++ src/components/utility/helmet.js
@@ -1,114 +1,114 @@
-export const defaultTitle = `KUclap : เว็บไซต์ค้นหาและแบ่งปันรีวิววิชาบูรณาการ มก.`;
-export const defaultDescription = `kuclap.com - แหล่งรวม ค้นหารีวิว เขียนรีวิว คำแนะนำ วิชาบูรณาการ วิชาบูร วิชาบูรฯ วิชาเสรี วิชาเลือก วิชาศึกษาทั่วไป รีวิว หน่วยกิต ชั่วโมงเรียน อาจารย์ การบ้าน ม.เกษตร มหาวิทยาลัยเกษตรศาสตร์ มก. KU - KUclap`;
-export const defaultImage = `https://www.kuclap.com/static/assets/img/meta-og-image.png`;
-export const defaultType = `website`;
-export const defaultURL = `https://www.kuclap.com/`;
+export const defaultTitle = `KUclap - เว็บไซต์ค้นหาและแบ่งปันรีวิววิชาบูรณาการ มก.`
+export const defaultDescription = `kuclap.com - แหล่งรวม ค้นหารีวิว เขียนรีวิว คำแนะนำ วิชาบูรณาการ วิชาบูร วิชาบูรฯ วิชาเสรี วิชาเลือก วิชาศึกษาทั่วไป รีวิว หน่วยกิต ชั่วโมงเรียน อาจารย์ การบ้าน ม.เกษตร มหาวิทยาลัยเกษตรศาสตร์ มก. KU - KUclap`
+export const defaultImage = `https://www.kuclap.com/static/assets/img/meta-og-image.png`
+export const defaultType = `website`
+export const defaultURL = `https://www.kuclap.com/`
 
 export const defaultMeta = {
-	title: defaultTitle,
-	description: defaultDescription
+    title: defaultTitle,
+    description: defaultDescription,
 }
 
 export const OpenGraphMeta = {
-	title: defaultTitle,
-	description: defaultDescription,
-	type: defaultType,
-	url: defaultURL,
-	image: defaultImage,
+    title: defaultTitle,
+    description: defaultDescription,
+    type: defaultType,
+    url: defaultURL,
+    image: defaultImage,
 }
 
 export const TwitterMeta = {
-	title: defaultTitle,
-	description: defaultDescription,
-	card: `summary_large_image`,
-	url: defaultURL,
-	image: defaultImage,
+    title: defaultTitle,
+    description: defaultDescription,
+    card: `summary_large_image`,
+    url: defaultURL,
+    image: defaultImage,
 }
 
 export const defaultPreRenderingData = {
-	...OpenGraphMeta,
-	...TwitterMeta
+    ...OpenGraphMeta,
+    ...TwitterMeta,
 }
 
 export const getHelmet = (page, subject, review) => {
-	switch (page) {
-		case "HOME": {
-			return {
-				title: defaultTitle,
-				description: defaultDescription,
-				image: defaultImage,
-			}
-		}
-		case "REVIEW":
-			return {
-				title: getTitle(page, subject, review),
-				description: getDescription(page, subject, review),
-				image: getImageURL(review),
-			}
-		case "CLASS":
-			return {
-				title: getTitle(page, subject, review),
-				description: getDescription(page, subject, review),
-				image: getImageURL(review),
-			}
-		default:
-			break;
-	}
-};
+    switch (page) {
+        case 'HOME': {
+            return {
+                title: defaultTitle,
+                description: defaultDescription,
+                image: defaultImage,
+            }
+        }
+        case 'REVIEW':
+            return {
+                title: getTitle(page, subject, review),
+                description: getDescription(page, subject, review),
+                image: getImageURL(review),
+            }
+        case 'CLASS':
+            return {
+                title: getTitle(page, subject, review),
+                description: getDescription(page, subject, review),
+                image: getImageURL(review),
+            }
+        default:
+            break
+    }
+}
 
 export const getTitle = (page, subject, review) => {
-	switch (page) {
-		case "REVIEW": {
-			if(review && subject){ 
-				return `${review.text} โดย ${review.author} - รีวิววิชา ${subject.nameTH} ${subject.nameEN} (รหัสวิชา ${subject.classID}) มก. - KUclap`
-			} else if (review) {
-				return `${review?.text || `กำลังโหลดข้อมูล`} โดย ${review?.author || `กำลังโหลดข้อมูล`} - รีวิววิชา ${review.classId} มก. - KUclap`
-			} else if (subject) {
-				return `ไม่มีข้อมูลรีวิวในระบบ - รีวิววิชา ${subject.nameTH} ${subject.nameEN} (รหัสวิชา ${subject.classID}) มก. - KUclap`
-			} 
-			return `ไม่มีข้อมูลรีวิวในระบบ - KUclap เว็บไซต์ค้นหาและแบ่งปันรีวิววิชาบูรณาการ มก.`
-		}
-		case "CLASS": {
-			if(subject){ 
-				return `รีวิววิชา ${subject.nameTH} ${subject.nameEN} (รหัสวิชา ${subject.classID}) มก. - KUclap`
-			} 
-			return `ไม่มีข้อมูลวิชาในระบบ - KUclap เว็บไซต์ค้นหาและแบ่งปันรีวิววิชาบูรณาการ มก.`
-		}
-		default: 
-			return defaultTitle
-	}
+    switch (page) {
+        case 'REVIEW': {
+            if (review && subject) {
+                return `${review.text} โดย ${review.author} - รีวิววิชา ${subject.nameTH} ${subject.nameEN} (รหัสวิชา ${subject.classID}) มก. - KUclap`
+            } else if (review) {
+                return `${review?.text || `กำลังโหลดข้อมูล`} โดย ${review?.author || `กำลังโหลดข้อมูล`} - รีวิววิชา ${
+                    review.classId
+                } มก. - KUclap`
+            } else if (subject) {
+                return `ไม่มีข้อมูลรีวิวในระบบ - รีวิววิชา ${subject.nameTH} ${subject.nameEN} (รหัสวิชา ${subject.classID}) มก. - KUclap`
+            }
+            return `ไม่มีข้อมูลรีวิวในระบบ - KUclap เว็บไซต์ค้นหาและแบ่งปันรีวิววิชาบูรณาการ มก.`
+        }
+        case 'CLASS': {
+            if (subject) {
+                return `รีวิววิชา ${subject.nameTH} ${subject.nameEN} (รหัสวิชา ${subject.classID}) มก. - KUclap`
+            }
+            return `ไม่มีข้อมูลวิชาในระบบ - KUclap เว็บไซต์ค้นหาและแบ่งปันรีวิววิชาบูรณาการ มก.`
+        }
+        default:
+            return defaultTitle
+    }
 }
-  
+
 export const getDescription = (page, subject, review) => {
-	switch (page) {
-		case "REVIEW": {
-			if(review && subject){ 
-				return `${review.text} โดย ${review.author} - รีวิววิชา ${subject.label} ${subject.nameEN} (รหัสวิชา ${subject.classID}) มก. - ${defaultDescription}`
-			} else if (review) {
-				return `${review.text} โดย ${review.author} - รีวิววิชา ${review.classId}) มก. - ${defaultDescription}`
-			} else if (subject) {
-				return `รีวิววิชา ${subject.nameTH} ${subject.nameEN} (รหัสวิชา ${subject.classID}) มก. - ${defaultDescription}`
-			} 
-			return defaultDescription
-		}
-		case "CLASS": {
-			if(subject){ 
-				return `รีวิววิชา ${subject.nameTH} ${subject.nameEN} (รหัสวิชา ${subject.classID}) มก. - ${defaultDescription}`
-			} 
-			return defaultDescription
-		}
-		default: 
-			return defaultDescription
-	}	
+    switch (page) {
+        case 'REVIEW': {
+            if (review && subject) {
+                return `${review.text} โดย ${review.author} - รีวิววิชา ${subject.label} ${subject.nameEN} (รหัสวิชา ${subject.classID}) มก. - ${defaultDescription}`
+            } else if (review) {
+                return `${review.text} โดย ${review.author} - รีวิววิชา ${review.classId}) มก. - ${defaultDescription}`
+            } else if (subject) {
+                return `รีวิววิชา ${subject.nameTH} ${subject.nameEN} (รหัสวิชา ${subject.classID}) มก. - ${defaultDescription}`
+            }
+            return defaultDescription
+        }
+        case 'CLASS': {
+            if (subject) {
+                return `รีวิววิชา ${subject.nameTH} ${subject.nameEN} (รหัสวิชา ${subject.classID}) มก. - ${defaultDescription}`
+            }
+            return defaultDescription
+        }
+        default:
+            return defaultDescription
+    }
 }
 
 export const getImageURL = (review) => {
-	if(review){
-		`https://og-image.kuclap.com/${encodeURIComponent(
-			review.text
-		)}.pngclassId=${review.classId}&classNameTH=${encodeURIComponent(
-			review.classNameTH
-		)}`
-	}
-	return defaultImage
+    if (review) {
+        return `https://og-image.kuclap.com/${encodeURIComponent(review.text)}.pngclassId=${
+            review.classId
+        }&classNameTH=${encodeURIComponent(review.classNameTH)}`
+    }
+    return defaultImage
 }
--- src/route/ClassPage.js
+++ src/route/ClassPage.js
@@ -1,260 +1,236 @@
-import { h } from "preact";
-import { useContext, useState } from "preact/hooks";
-import styled, { css, withTheme } from "styled-components";
+import { h } from 'preact'
+import { useContext, useState } from 'preact/hooks'
+import styled, { css, withTheme } from 'styled-components'
 
-import { getHelmet } from "../components/utility/helmet";
-import { NoMoreReview, NoReview, HomeIcon, NoMoreQuestion, NoQuestion } from "../components/utility/Icons";
-import Footer from "../components/common/Footer";
-import PageTemplate from "../components/common/PageTemplate";
-import ReviewCard from "../components/common/ReviewCard";
-import QuestionCard from "../components/common/QuestionCard";
+import { getHelmet } from '../components/utility/helmet'
+import { NoMoreReview, NoReview, HomeIcon, NoMoreQuestion, NoQuestion } from '../components/utility/Icons'
+import Footer from '../components/common/Footer'
+import PageTemplate from '../components/common/PageTemplate'
+import ReviewCard from '../components/common/ReviewCard'
+import QuestionCard from '../components/common/QuestionCard'
+
+import { ReviewSkeletonA, ReviewSkeletonB } from '../components/common/ReviewSkeleton'
+
+import Details from '../components/common/Detail'
+import {
+    navigateToHomePage,
+    navigateToFormReviewPage,
+    getClassName,
+    getColorHash,
+    getDetailFromLabel,
+} from '../components/utility/helper'
+
+import { SelectContext } from '../context/SelectContext'
 
 import {
-	ReviewSkeletonA,
-	ReviewSkeletonB,
-} from "../components/common/ReviewSkeleton";
-
-import Details from "../components/common/Detail";
-import {
-  navigateToHomePage,
-  navigateToFormReviewPage,
-  getClassName,
-  getColorHash,
-  getDetailFromLabel,
-} from "../components/utility/helper";
-
-import { SelectContext } from "../context/SelectContext";
-
-import {
-	DetailTitle,
-	AdaptorReviews,
-	LastReview,
-	ContainerNoMore,
-	NoMoreCustom,
-	SubjectTitle,
-} from "../components/common/FetcherComponents";
-import { PrimaryButton, SecondaryButton } from "../components/common/DesignSystemStyles";
-import QuestionModal from "../components/common/QuestionModal";
-import { FetcherContext, FetcherProvider } from "../context/FetcherContext";
-import { route } from "preact-router";
+    DetailTitle,
+    AdaptorReviews,
+    LastReview,
+    ContainerNoMore,
+    NoMoreCustom,
+    SubjectTitle,
+} from '../components/common/FetcherComponents'
+import { PrimaryButton, SecondaryButton } from '../components/common/DesignSystemStyles'
+import QuestionModal from '../components/common/QuestionModal'
+import { FetcherContext, FetcherProvider } from '../context/FetcherContext'
+import { route } from 'preact-router'
 
 const Button = styled.div`
-  background-color: #2f80ed;
-  color: #fff;
-  padding: 0.3rem 1.8rem;
-  border-radius: 0.4rem;
-  font-size: 1.6rem;
-  display: flex;
-  align-items: center;
-  cursor: pointer;
+    background-color: #2f80ed;
+    color: #fff;
+    padding: 0.3rem 1.8rem;
+    border-radius: 0.4rem;
+    font-size: 1.6rem;
+    display: flex;
+    align-items: center;
+    cursor: pointer;
 `
 
 const ButtonLastReview = styled(SecondaryButton)`
-	margin-right: 1rem;
-	box-shadow: inset 0 0 0 0.1rem ${props => props.theme.subText};
-	padding: 0.2rem 1rem;
-	align-self: unset;
+    margin-right: 1rem;
+    box-shadow: inset 0 0 0 0.1rem ${(props) => props.theme.subText};
+    padding: 0.2rem 1rem;
+    align-self: unset;
 
-	svg {
-		path {
-		stroke: ${props => props.theme.subText}
-		}
-	}
+    svg {
+        path {
+            stroke: ${(props) => props.theme.subText};
+        }
+    }
 
-	&:hover {
-		background: ${(props) => props.theme.lightBackground};
-	}
-`;
+    &:hover {
+        background: ${(props) => props.theme.lightBackground};
+    }
+`
 
 const ContainerBtns = styled.div`
-  margin: 0;
-  display: flex;
-`;
+    margin: 0;
+    display: flex;
+`
 
 const ReviewTitle = styled.div`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-top: 2.2rem;
-`;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-top: 2.2rem;
+`
 
 const TabTitle = styled(DetailTitle)`
-	padding: 0 1rem;
+    padding: 0 1rem;
     cursor: pointer;
-	color: ${props => props.isActive ? props.theme.mainText : "#BFBFBF"};
+    color: ${(props) => (props.isActive ? props.theme.mainText : '#BFBFBF')};
 `
 
 const SectionTabButton = styled.div`
-	position: relative;
+    position: relative;
 `
 
 const TabLine = styled.div`
-  width: 100%;
-  height: 3px;
-  border-radius: 50px;
-  background-color: ${props => props.theme.borderColor};
-  position: absolute;
-  
-  &:last-child {
-	background-color: #2f80ed;
-	transition: all 0.3s ease-in-out;
-	${props => props.underlineAt === "review" ? css`
-		width: 10.4rem;
-		left: 0;
-	` : css`
-		width: 12.6rem;
-		left: 4.8rem;
-	`}
-  }
+    width: 100%;
+    height: 3px;
+    border-radius: 50px;
+    background-color: ${(props) => props.theme.borderColor};
+    position: absolute;
+
+    &:last-child {
+        background-color: #2f80ed;
+        transition: all 0.3s ease-in-out;
+        ${(props) =>
+            props.underlineAt === 'review'
+                ? css`
+                      width: 10.4rem;
+                      left: 0;
+                  `
+                : css`
+                      width: 12.6rem;
+                      left: 4.8rem;
+                  `}
+    }
 `
 
 const ClassPage = (props) => {
-  const { classID, fetchTarget, setFetchTarget } = props;
-  const { state: selected } = useContext(SelectContext);
-  const {
-    reviews,
-    score,
-    loading,
-    underflow,
-    loadMore,
-    setUnderFlow,
-	questions
-  } = useContext(FetcherContext)
-  const [showQuestionModal, setQuestionModal] = useState(false)
+    const { classID, fetchTarget, setFetchTarget } = props
+    const { state: selected } = useContext(SelectContext)
+    const { reviews, score, loading, underflow, loadMore, setUnderFlow, questions } = useContext(FetcherContext)
+    const [showQuestionModal, setQuestionModal] = useState(false)
 
-  console.log({loading, loadMore, underflow, questions})
+    console.log({ loading, loadMore, underflow, questions })
 
+    const handleNewReview = () => {
+        navigateToFormReviewPage(classID)
+        setUnderFlow(false)
+    }
 
-  const handleNewReview = () => {
-    navigateToFormReviewPage(classID)
-    setUnderFlow(false);
-  };
+    const changeTab = (target) => {
+        setFetchTarget(target)
+        if (target !== 'review') {
+            route(`/${classID}?display=${target}`)
+        } else {
+            route(`/${classID}`)
+        }
+    }
 
-  const changeTab = (target) => {
-	setFetchTarget(target)
-	if (target !== "review") {
-		route(`/${classID}?display=${target}`)
-	} else {
-		route(`/${classID}`)
-	}
-  }
-
-  return (
-    <PageTemplate
-      content={getHelmet("CLASS", getDetailFromLabel(selected.label))}
-      {...props}
-    >
-      <SubjectTitle color={getColorHash(classID)}>
-        <span>{classID}</span>
-        {selected.label === "ค้นหาวิชาด้วยรหัสวิชา ชื่อวิชาภาษาไทย / ภาษาอังกฤษ"
-          ? "กำลังโหลดข้อมูลวิชา..."
-          : getClassName(selected.label)}
-      </SubjectTitle>
-      <Details score={score} />
-      <LastReview>
-        <ReviewTitle>
-			<SectionTabButton>
-				<TabTitle as="button" isActive={fetchTarget === "review"} onClick={() => changeTab("review")}>
-					{fetchTarget === "review" ? "รีวิวทั้งหมด" : "รีวิว"}
-				</TabTitle>
-				<TabTitle as="button" isActive={fetchTarget === "question"} onClick={() => changeTab("question")}>
-					{fetchTarget === "question" ? "คำถามทั้งหมด" : "คำถาม"}
-				</TabTitle>
-				<TabLine />
-				<TabLine underlineAt={fetchTarget} />
-			</SectionTabButton>
-			<ContainerBtns>
-				<ButtonLastReview onClick={navigateToHomePage}>
-				<HomeIcon  />
-				</ButtonLastReview>
-				{
-					fetchTarget === "review" ?
-					<Button onClick={handleNewReview}>รีวิววิชานี้</Button> 
-					: <Button onClick={() => setQuestionModal(true)}>ถามคำถาม</Button>
-				}
-			</ContainerBtns>
-        </ReviewTitle>
-		<QuestionModal 
-			classID={classID}
-			className={selected.label}
-			showQuestionModal={showQuestionModal}
-			setQuestionModal={setQuestionModal}
-		/>
-		{ fetchTarget === "review" ?
-			<AdaptorReviews id="adaptor">
-				{reviews?.map(
-					(review, index) =>
-						review && (
-							<ReviewCard
-								key={index}
-								isBadge={false}
-								currentRoute={"CLASS"}
-								{...review}
-							/>
-						)
-					)
-				}
-			</AdaptorReviews> :
-			<AdaptorReviews id="adaptor-question">
-				{questions?.map(
-					(question, index) => 
-						question && (
-							<QuestionCard 
-								key={index}
-								questionInfo={question}
-							/>
-						)
-					)
-				}
-			</AdaptorReviews>
-		}
-        {(loading || loadMore) && !underflow ? (
-          <>
-            <ReviewSkeletonA />
-            <ReviewSkeletonB />
-          </>
-        ) : (fetchTarget === "review" ? (
-          reviews &&
-          !loading &&
-          underflow && (
-            <>
-              <ContainerNoMore>
-                <NoMoreCustom>
-                  {reviews.length > 0 ? <NoMoreReview /> : <NoReview />}
-                </NoMoreCustom>
-                <PrimaryButton onClick={handleNewReview}>เพิ่มรีวิว</PrimaryButton>
-              </ContainerNoMore>
-              <Footer />
-            </>
-          )
-        ) : (questions &&
-			!loading &&
-			underflow && (
-			<>
-				<ContainerNoMore>
-				<NoMoreCustom>
-					{questions.length > 0 ? <NoMoreQuestion /> : <NoQuestion />}
-				</NoMoreCustom>
-				<PrimaryButton onClick={() => setQuestionModal(true)}>ถามคำถาม</PrimaryButton>
-				</ContainerNoMore>
-				<Footer />
-			</>
-			)
-		))}
-      </LastReview>
-    </PageTemplate>
-  );
-};
+    return (
+        <PageTemplate content={getHelmet('CLASS', getDetailFromLabel(selected.label))} {...props}>
+            <SubjectTitle color={getColorHash(classID)}>
+                <span>{classID}</span>
+                {selected.label === 'ค้นหาวิชาด้วยรหัสวิชา ชื่อวิชาภาษาไทย / ภาษาอังกฤษ'
+                    ? 'กำลังโหลดข้อมูลวิชา...'
+                    : getClassName(selected.label)}
+            </SubjectTitle>
+            <Details score={score} />
+            <LastReview>
+                <ReviewTitle>
+                    <SectionTabButton>
+                        <TabTitle as="button" isActive={fetchTarget === 'review'} onClick={() => changeTab('review')}>
+                            {fetchTarget === 'review' ? 'รีวิวทั้งหมด' : 'รีวิว'}
+                        </TabTitle>
+                        <TabTitle
+                            as="button"
+                            isActive={fetchTarget === 'question'}
+                            onClick={() => changeTab('question')}>
+                            {fetchTarget === 'question' ? 'คำถามทั้งหมด' : 'คำถาม'}
+                        </TabTitle>
+                        <TabLine />
+                        <TabLine underlineAt={fetchTarget} />
+                    </SectionTabButton>
+                    <ContainerBtns>
+                        <ButtonLastReview onClick={navigateToHomePage}>
+                            <HomeIcon />
+                        </ButtonLastReview>
+                        {fetchTarget === 'review' ? (
+                            <Button onClick={handleNewReview}>รีวิววิชานี้</Button>
+                        ) : (
+                            <Button onClick={() => setQuestionModal(true)}>ถามคำถาม</Button>
+                        )}
+                    </ContainerBtns>
+                </ReviewTitle>
+                <QuestionModal
+                    classID={classID}
+                    className={selected.label}
+                    showQuestionModal={showQuestionModal}
+                    setQuestionModal={setQuestionModal}
+                />
+                {fetchTarget === 'review' ? (
+                    <AdaptorReviews id="adaptor">
+                        {reviews?.map(
+                            (review, index) =>
+                                review && <ReviewCard key={index} isBadge={false} currentRoute={'CLASS'} {...review} />,
+                        )}
+                    </AdaptorReviews>
+                ) : (
+                    <AdaptorReviews id="adaptor-question">
+                        {questions?.map(
+                            (question, index) => question && <QuestionCard key={index} questionInfo={question} />,
+                        )}
+                    </AdaptorReviews>
+                )}
+                {(loading || loadMore) && !underflow ? (
+                    <>
+                        <ReviewSkeletonA />
+                        <ReviewSkeletonB />
+                    </>
+                ) : fetchTarget === 'review' ? (
+                    reviews &&
+                    !loading &&
+                    underflow && (
+                        <>
+                            <ContainerNoMore>
+                                <NoMoreCustom>{reviews.length > 0 ? <NoMoreReview /> : <NoReview />}</NoMoreCustom>
+                                <PrimaryButton onClick={handleNewReview}>เพิ่มรีวิว</PrimaryButton>
+                            </ContainerNoMore>
+                            <Footer />
+                        </>
+                    )
+                ) : (
+                    questions &&
+                    !loading &&
+                    underflow && (
+                        <>
+                            <ContainerNoMore>
+                                <NoMoreCustom>
+                                    {questions.length > 0 ? <NoMoreQuestion /> : <NoQuestion />}
+                                </NoMoreCustom>
+                                <PrimaryButton onClick={() => setQuestionModal(true)}>ถามคำถาม</PrimaryButton>
+                            </ContainerNoMore>
+                            <Footer />
+                        </>
+                    )
+                )}
+            </LastReview>
+        </PageTemplate>
+    )
+}
 
 const Interface = (props) => {
-	const { classID } = props;
-	const [fetchTarget, setFetchTarget] = useState(props.matches.display || "review")
+    const { classID } = props
+    const [fetchTarget, setFetchTarget] = useState(props.matches.display || 'review')
 
-	return (
-		<FetcherProvider classID={classID} fetchTarget={fetchTarget}>
-			<ClassPage {...props} fetchTarget={fetchTarget} setFetchTarget={setFetchTarget} />
-		</FetcherProvider>
-	);
-};
+    return (
+        <FetcherProvider classID={classID} fetchTarget={fetchTarget}>
+            <ClassPage {...props} fetchTarget={fetchTarget} setFetchTarget={setFetchTarget} />
+        </FetcherProvider>
+    )
+}
 
-export default withTheme(Interface);
+export default withTheme(Interface)
