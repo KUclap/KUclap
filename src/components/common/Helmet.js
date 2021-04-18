9be086a7cd0a700fccaa4d2a74b1219f8d9f25be
aslupin <poon_arsene_lupin@hotmail.com>
2021-04-18 22:58:23 +0700

    [-] fix : delete amp feature

--- src/components/common/Helmet.js
+++ src/components/common/Helmet.js
@@ -1,42 +1,42 @@
-import { h } from 'preact'
-import Helmet from 'preact-helmet'
+import { h } from "preact";
+import Helmet from "preact-helmet";
 
 const HelmetComponent = ({ content }) => {
-    return (
-        <Helmet
-            htmlAttributes={{ lang: 'th', amp: undefined }} // amp takes no value
-            title="KUclap"
-            titleTemplate={content.title}
-            defaultTitle="KUclap - เว็บไซต์ค้นหาและแบ่งปันรีวิววิชาบูรณาการ มก."
-            titleAttributes={{ itemprop: 'name', lang: 'th' }}
-            //base={{ target: "_blank", href: "https://www.kuclap.com/" }}
-            meta={[
-                // Robot
-                // { name: "robots", content: "noindex" },
-                // Primary Meta Tags
-                { name: 'title', content: `${content.title}` },
-                { name: 'description', content: `${content.description}` },
-                // Open Graph / Facebook
-                { property: 'og:type', content: 'website' },
-                { property: 'og:url', content: 'https://www.kuclap.com/' },
-                { property: 'og:title', content: `KUclap - ${content.title}` },
-                {
-                    property: 'og:description',
-                    content: `${content.description}`,
-                },
-                { property: 'og:image', content: content.image },
-                // Twitter
-                { property: 'twitter:card', content: 'summary_large_image' },
-                { property: 'twitter:url', content: 'https://www.kuclap.com/' },
-                { property: 'twitter:title', content: `KUclap - ${content.title}` },
-                {
-                    property: 'twitter:description',
-                    content: `${content.description}`,
-                },
-                { property: 'twitter:image', content: content.image },
-            ]}
-        />
-    )
-}
+	return (
+		<Helmet
+			htmlAttributes={{ lang: "th" }} // amp takes no value
+			title="KUclap"
+			titleTemplate={content.title}
+			defaultTitle="KUclap - เว็บไซต์ค้นหาและแบ่งปันรีวิววิชาบูรณาการ มก."
+			titleAttributes={{ itemprop: "name", lang: "th" }}
+			//base={{ target: "_blank", href: "https://www.kuclap.com/" }}
+			meta={[
+				// Robot
+				// { name: "robots", content: "noindex" },
+				// Primary Meta Tags
+				{ name: "title", content: `${content.title}` },
+				{ name: "description", content: `${content.description}` },
+				// Open Graph / Facebook
+				{ property: "og:type", content: "website" },
+				{ property: "og:url", content: "https://www.kuclap.com/" },
+				{ property: "og:title", content: `KUclap - ${content.title}` },
+				{
+					property: "og:description",
+					content: `${content.description}`,
+				},
+				{ property: "og:image", content: content.image },
+				// Twitter
+				{ property: "twitter:card", content: "summary_large_image" },
+				{ property: "twitter:url", content: "https://www.kuclap.com/" },
+				{ property: "twitter:title", content: `KUclap - ${content.title}` },
+				{
+					property: "twitter:description",
+					content: `${content.description}`,
+				},
+				{ property: "twitter:image", content: content.image },
+			]}
+		/>
+	);
+};
 
-export default HelmetComponent
+export default HelmetComponent;
