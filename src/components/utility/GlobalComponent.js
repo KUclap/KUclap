e564da3c3c7a073d3a02abec5ac695a6165764ab
aslupin <poon_arsene_lupin@hotmail.com>
2021-04-25 00:23:19 +0700

    [+] fix : loading font one time at root html

--- src/components/utility/GlobalComponent.js
+++ src/components/utility/GlobalComponent.js
@@ -1,6 +1,7 @@
 import { h } from "preact";
-import { useContext, useEffect, useState } from 'preact/hooks'
+import { useContext, useEffect, useState } from "preact/hooks";
 import { createGlobalStyle, withTheme } from "styled-components";
+
 import { ModalContext } from "../../context/ModalContext";
 
 const GlobalStyles = createGlobalStyle`
@@ -14,23 +15,19 @@ body {
 `;
 
 const GlobalComponent = () => {
-  const [mounted, setMounted] = useState(false)
-  const { state: modal } = useContext(ModalContext);
+	const [mounted, setMounted] = useState(false);
+	const { state: modal } = useContext(ModalContext);
 
-  useEffect(() => {
-    setMounted(true)
-  }, [])
+	useEffect(() => {
+		setMounted(true);
+	}, []);
 
-  return (
-    <>
-      <link
-        href="https://fonts.googleapis.com/css2?family=Kanit:wght@300;400;500;600&display=swap"
-        rel="stylesheet"
-      />
-      { mounted && <GlobalStyles isOverflow={modal.showModal} /> } 
-      {/* <GlobalStyles isOverflow={modal.showModal} />  */}
-    </>
-  );
+	return (
+		<>
+			{mounted && <GlobalStyles isOverflow={modal.showModal} />}
+			{/* <GlobalStyles isOverflow={modal.showModal} />  */}
+		</>
+	);
 };
 
 export default withTheme(GlobalComponent);
