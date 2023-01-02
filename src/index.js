de463d59a82c37f949f8b4c2fd4fe1391357d409
aslupin <poon_arsene_lupin@hotmail.com>
2023-01-02 22:31:44 +0700

    [+] add: setup Sentry

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ import { BrowserTracing } from "@sentry/tracing";
 Sentry.init({
   dsn: "https://7f0d00fb2bb84ad5a3fc961253482a93@o4504433520082944.ingest.sentry.io/4504433608425472",
   integrations: [new BrowserTracing()],
+  environment: process.env.NODE_ENV || 'unknown',
 
   // Set tracesSampleRate to 1.0 to capture 100%
   // of transactions for performance monitoring.
